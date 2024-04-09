19ed75cc1f29a5996b0f4e0855cc1212ce01f800
Christopher Kirathe <chrisk720@Christophers-MacBook-Pro.local>
2024-04-09 08:40:51 +0300

    done with Lab there was only 1 test though

--- server/app.py
+++ server/app.py
@@ -39,33 +39,26 @@ def games():
 
     return response
 
-@app.route('/games/<int:id>')
+@app.route('/games/<int:id>', methods=['GET','DELETE'])
 def game_by_id(id):
     game = Game.query.filter(Game.id == id).first()
+    if not game:
+        return make_response({'message':f'There is no game with an id of {id}'},404)
     
-    game_dict = game.to_dict()
+    if request.method == 'GET':
+        game_dict = game.to_dict()
 
-    response = make_response(
-        game_dict,
-        200
-    )
-
-    return response
-
-@app.route('/reviews')
-def reviews():
-
-    reviews = []
-    for review in Review.query.all():
-        review_dict = review.to_dict()
-        reviews.append(review_dict)
-
-    response = make_response(
-        reviews,
-        200
-    )
+        response = make_response(
+            game_dict,
+            200
+        )
+    
+        
+        
 
     return response
+
+
 
 @app.route('/users')
 def users():
@@ -81,6 +74,60 @@ def users():
     )
 
     return response
+
+@app.route('/reviews', methods=['GET', 'POST'])
+def reviews():
+    reviews = Review.query.all()
+    if not reviews:
+        return make_response({'message':'There are no reviews'})
+    if request.method == 'GET':
+        reviews_to_return = [review.to_dict() for review in reviews]
+        return make_response(reviews_to_return,200)
+    elif request.method == 'POST':
+        new_review = Review(
+            score= request.form.get('score'),
+            comment= request.form.get('comment'),
+            game_id= request.form.get('game_id'),
+            user_id= request.form.get('user_id')
+        )
+
+        db.session.add(new_review)
+        db.session.commit()
+        
+        review_dict = new_review.to_dict()
+        return make_response(review_dict, 201)
+
+@app.route('/reviews/<int:id>', methods=['GET','DELETE','POST','Patch'])
+def review_by_id(id):
+    review = Review.query.filter_by(id = id).first()
+    if not review:
+        return make_response({'message':f'There is no review with an id of {id}'}, 404)
+    if request.method == 'GET':
+        return make_response(review.to_dict(),200)
+    elif request.method == 'DELETE':
+        db.session.delete(review)
+        db.session.commit()
+        response = {
+            'successfuly_deleted':True,
+            'message':'review has been deleted'
+        }
+        return make_response(response,200)
+    # I want to do a patch
+    # I need to use the review which is the specific review
+    # then I need acces the data in the request
+    # update the review with it
+    # commit the cahnges then return the updated review in a JSON response
+    elif request.method == 'PATCH':
+        # setattr(object, attribute, value)
+        # we use setattr because we do not know which fields are being updated from the frontend very important
+        for attr in request.form:
+            setattr(review,attr,request.form.get(attr))
+        db.session.commit()
+        review_dict = review.to_dict()
+        return make_response(review_dict,200)
+        
+
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
